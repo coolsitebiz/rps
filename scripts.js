b03b3fea5a40a090a1246486ebb54d1a80829fe9
bryan <bryan@coolsite.biz>
2022-08-31 23:55:26 -0700

    Change console logging to ui display in divs

--- scripts.js
+++ scripts.js
@@ -20,65 +20,77 @@ function getPlayerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
-    console.log("player: " + playerChoice);
-    console.log("computer: " + computerChoice);
+    let outcome = {
+        compChoice: computerChoice,
+        result: ""
+    }
 
     if (playerChoice === computerChoice) {
-        return "tie";
+        outcome.result = "tie";
+        return outcome;
     }
     switch (playerChoice) {
         case ("rock"):
             if (computerChoice === "scissors") {
-                return "win";
+                outcome.result = "win";
+
             }
             else if (computerChoice === "paper") {
-                return "lose";
+                outcome.result = "lose";
+
             }
         case ("scissors"):
             if (computerChoice === "paper") {
-                return "win";
+                outcome.result = "win";
+
             }
             else if (computerChoice === "rock") {
-                return "lose";
+                outcome.result = "lose";
+
             }
         case ("paper"):
             if (computerChoice === "rock") {
-                return "win";
+                outcome.result = "win";
+
             }
             else if (computerChoice === "scissors") {
-                return "lose";
+                outcome.result = "lose";
+
             }
-        case ("quit"): 
-            return "lose";
-        
+        case ("quit"):
+            outcome.result = "lose";
+
+        default:
+            return outcome;
     }
 }
 
-function game() {
-    let score = [0,0];
-    for (i = 0; i < 5; i++) {
-        let result = playRound(getPlayerChoice(), getComputerChoice());
-        if (result === "win") {
-            console.log("You win!")
-            score[0]++;
-        }
-        else if (result === "lose"){
-            console.log("You lose.")
-            score[1]++;
-        }
-        else {
-            console.log("Tie game.")
-        }
+function game(playerChoice) {
+    //let score = [0, 0];
+    let resultsDiv = document.querySelector('#results');
+    let choiceDiv = document.querySelector('#choice');
+
+    let result = playRound(playerChoice, getComputerChoice());
+    choiceDiv.textContent = `Computer chose ${result.compChoice}.`
+    if (result.result === "win") {
+        resultsDiv.textContent = `You win!`;
+        //score[0]++;
     }
-    console.log("Your score: " + score[0]);
-    console.log("CPU score: " + score[1]);
-    if (score[0] === score[1]) {
-        console.log("TIE GAME");
-    } else if (score[0] > score[1]) {
-        console.log("You win!");
-    } else {
-        console.log("CPU wins!");
+    else if (result.result === "lose") {
+        resultsDiv.textContent = `You lose!`;
+        //score[1]++;
     }
+    else {
+        resultsDiv.textContent = `Tie game!`;
+    }
+
+    // if (score[0] === score[1]) {
+    //     console.log("TIE GAME");
+    // } else if (score[0] > score[1]) {
+    //     console.log("You win!");
+    // } else {
+    //     console.log("CPU wins!");
+    // }
 
 
 }
