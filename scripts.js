cd7777dff3076ad2c44e745a0acad8d063786824
Bryan Nelson <32003096+coolsitebiz@users.noreply.github.com>
2022-09-02 12:45:55 -0700

    Add images and styling.

--- scripts.js
+++ scripts.js
@@ -1,96 +1,81 @@
 function getComputerChoice() {
-    let choices = ['rock', 'paper', 'scissors'];
-    let selection = choices[Math.floor(Math.random() * 3)];
-    return selection;
+  let choices = ["rock", "paper", "scissors"];
+  let selection = choices[Math.floor(Math.random() * 3)];
+  return selection;
 }
 
 function getPlayerChoice() {
-    let choice = prompt("Choose your weapon (rock, paper, or scissors):");
-    if (!choice) {
-        console.log("I guess you don't want to play.");
-        return "quit";
-    }
-    choice = choice.toLowerCase();
-    if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-        return getPlayerChoice();
-    } else {
-        return choice;
-    }
-
+  let choice = prompt("Choose your weapon (rock, paper, or scissors):");
+  if (!choice) {
+    console.log("I guess you don't want to play.");
+    return "quit";
+  }
+  choice = choice.toLowerCase();
+  if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+    return getPlayerChoice();
+  } else {
+    return choice;
+  }
 }
 
 function playRound(playerChoice, computerChoice) {
-    let outcome = {
-        compChoice: computerChoice,
-        result: ""
-    }
+  let outcome = {
+    compChoice: computerChoice,
+    result: "",
+  };
 
-    if (playerChoice === computerChoice) {
-        outcome.result = "tie";
-        return outcome;
-    }
-    switch (playerChoice) {
-        case ("rock"):
-            if (computerChoice === "scissors") {
-                outcome.result = "win";
-
-            }
-            else if (computerChoice === "paper") {
-                outcome.result = "lose";
-
-            }
-        case ("scissors"):
-            if (computerChoice === "paper") {
-                outcome.result = "win";
-
-            }
-            else if (computerChoice === "rock") {
-                outcome.result = "lose";
-
-            }
-        case ("paper"):
-            if (computerChoice === "rock") {
-                outcome.result = "win";
-
-            }
-            else if (computerChoice === "scissors") {
-                outcome.result = "lose";
-
-            }
-        case ("quit"):
-            outcome.result = "lose";
-
-        default:
-            return outcome;
-    }
+  if (playerChoice === computerChoice) {
+    outcome.result = "tie";
+    return outcome;
+  }
+  switch (playerChoice) {
+    case "rock":
+      if (computerChoice === "scissors") {
+        outcome.result = "win";
+      } else if (computerChoice === "paper") {
+        outcome.result = "lose";
+      }
+      return outcome;
+    case "scissors":
+      if (computerChoice === "paper") {
+        outcome.result = "win";
+      } else if (computerChoice === "rock") {
+        outcome.result = "lose";
+      }
+      return outcome;
+    case "paper":
+      if (computerChoice === "rock") {
+        outcome.result = "win";
+      } else if (computerChoice === "scissors") {
+        outcome.result = "lose";
+      }
+      return outcome;
+  }
 }
 
 function game(playerChoice) {
-    //let score = [0, 0];
-    let resultsDiv = document.querySelector('#results');
-    let choiceDiv = document.querySelector('#choice');
+  //let score = [0, 0];
+  let resultsDiv = document.querySelector("#results");
+  let choiceDiv = document.querySelector("#choice");
 
-    let result = playRound(playerChoice, getComputerChoice());
-    choiceDiv.textContent = `Computer chose ${result.compChoice}.`
-    if (result.result === "win") {
-        resultsDiv.textContent = `You win!`;
-        //score[0]++;
-    }
-    else if (result.result === "lose") {
-        resultsDiv.textContent = `You lose!`;
-        //score[1]++;
-    }
-    else {
-        resultsDiv.textContent = `Tie game!`;
-    }
+  let result = playRound(playerChoice, getComputerChoice());
+  choiceDiv.textContent = `Computer chose ${result.compChoice.toUpperCase()}.`;
+  console.log(result.result);
+  if (result.result === "win") {
+    resultsDiv.textContent = `You win!`;
+    //score[0]++;
+  } else if (result.result === "lose") {
+    resultsDiv.textContent = `You lose!`;
+    //score[1]++;
+  } else {
+    resultsDiv.textContent = `Tie game!`;
+  }
 
-    // if (score[0] === score[1]) {
-    //     console.log("TIE GAME");
-    // } else if (score[0] > score[1]) {
-    //     console.log("You win!");
-    // } else {
-    //     console.log("CPU wins!");
-    // }
-
-
+  // if (score[0] === score[1]) {
+  //     console.log("TIE GAME");
+  // } else if (score[0] > score[1]) {
+  //     console.log("You win!");
+  // } else {
+  //     console.log("CPU wins!");
+  // }
 }
